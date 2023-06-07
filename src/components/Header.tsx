31943d03379775389cf9d466b71ed2bf37e75fd5
Evelina Norlin <evelina.norlin@hotmail.com>
2023-06-07 11:32:49 +0200

    added html to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,51 @@
 export default function Header(){
   return(
-    <h1>header</h1>
+    <section id="hero" className="hero">
+    <img
+      src="public/eve.svg"
+      className="eve"
+      width="200"
+      height="300"
+      loading="lazy"
+    />
+    <img
+      src="public/lina.svg"
+      className="lina"
+      width="300"
+      height="300"
+      loading="lazy"
+    />
+    <img
+      src="public/evelina.svg"
+      className="evelina"
+      width="700"
+      height="300"
+      loading="lazy"
+    />
+    <div className="hero-flex">
+      <img
+        src="public/evelina_norlin.jpg"
+        width="300"
+        height="400"
+        alt="image og blonde woman"
+        loading="lazy"
+        className="profile-picture"
+      />
+      <div>
+        <h1>
+          Frilansande <span>webbutvecklare</span> med passion för funktionell
+          <span>kod</span> & krispig <span>design</span>.
+        </h1>
+        <p className="about">
+          Just nu läser till front-end utvecklare. Vid sidan av mina studier
+          läser jag kurser i grafisk design och UX-design. När jag inte sitter
+          vid datorn och skapar hittar du mig i yogasalen, på en klätterklippa
+          eller ute till havs.
+        </p>
+        <button className="dark-button">jobba med mig</button>
+        <button className="dark-button">portfolio</button>
+      </div>
+    </div>
+  </section>
   )
 }
